734785cf9a173960d547a231bb9e727163aa68ef
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-14 23:16:17 +0100

    Add main style

---
+++ src/homepage.js
@@ -0,0 +1,10 @@
+const outputDiv = document.querySelector("#content");
+
+const heroHeading = document.createElement("h2"); 
+heroHeading.textContent = "Taste That Feels Like Home";
+
+const heroDescription = document.createElement("p");
+
+const heroCTA = document.createElement("p");
+
+outputDiv.appendChild(heroHeading);
