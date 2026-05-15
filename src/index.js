53b22a9dedd5351265e91390fdfe2a29cdaede65
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-15 15:34:33 +0100

    Invoke function to populate display onload

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { home } from "./home.js";
 const display = document.querySelector("#content");
 const navButtons = document.querySelectorAll("nav button");
 
+home.displayContent(display);
+
 navButtons.forEach(button => {
   button.addEventListener('click', e => {
     e.preventDefault();
+    
     if (button.dataset.id == "home") {
       display.textContent = "";
       home.displayContent(display);
+      return;
     }
   });
 });
