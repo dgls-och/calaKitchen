c84766fdec27a13652c1f0083ca390fcd48c3144
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-15 16:40:43 +0100

    Add classes to menu items for styling

--- src/menu.js
+++ src/menu.js
@@ -7,15 +7,19 @@ const dish1 = document.createElement("div");
 dish1.classList.add("dish");
 
 const dish1Heading = document.createElement("h2");
+dish1Heading.classList.add("dish-h2");
 dish1Heading.textContent = "Smoky Jollof Royale";
 
 const dish1Description = document.createElement("p");
+dish1Description.classList.add("dish-p");
 dish1Description.textContent = "A rich, firewood-style Jollof rice bursting with bold tomato and spice flavours, served with juicy grilled chicken and sweet fried platains. A true tatse of Nigeria in every bite.";
 
 const dish1Price = document.createElement("p");
+dish1Price.classList.add("bold", "dish-p");
 dish1Price.textContent = "NGN 5,500";
 
 const dish1OrderButton = document.createElement("button");
+dish1OrderButton.classList.add("menu-button", "content-nav-button")
 dish1OrderButton.textContent = "Order now";
 
 dish1.appendChild(dish1Heading);
@@ -29,15 +33,19 @@ const dish2 = document.createElement("div");
 dish2.classList.add("dish");
 
 const dish2Heading = document.createElement("h2");
-dish2Heading.textContent = "Swahili Coocnut Chicken";
+dish2Heading.classList.add("dish-h2");
+dish2Heading.textContent = "Swahili Coconut Chicken";
 
 const dish2Description = document.createElement("p");
+dish2Description.classList.add("dish-p");
 dish2Description.textContent = "Tender chicken simmered on a creamy coconut sauce infused with East African spices, served  with fragrant rice. Smooth, comforting, and deeply satisfying.";
 
 const dish2Price = document.createElement("p");
+dish2Price.classList.add("bold", "dish-p");
 dish2Price.textContent = "NGN 6,200";
 
 const dish2OrderButton = document.createElement("button");
+dish2OrderButton.classList.add("menu-button", "content-nav-button")
 dish2OrderButton.textContent = "Order now";
 
 dish2.appendChild(dish2Heading);
@@ -50,15 +58,19 @@ const dish3 = document.createElement("div");
 dish3.classList.add("dish");
 
 const dish3Heading = document.createElement("h2");
+dish3Heading.classList.add("dish-h2");
 dish3Heading.textContent = "Asian Stir-Fry Delight";
 
 const dish3Description = document.createElement("p");
+dish3Description.classList.add("dish-p");
 dish3Description.textContent = "A vibrant mix of stir-fried noodles, crisp vegetables, and your choice of chicken or beef, tossed in a savory soy-garlic sauce. Bold flavours with a perfect balance.";
 
 const dish3Price = document.createElement("p");
+dish3Price.classList.add("bold", "dish-p");
 dish3Price.textContent = "NGN 6,800";
 
 const dish3OrderButton = document.createElement("button");
+dish3OrderButton.classList.add("menu-button", "content-nav-button")
 dish3OrderButton.textContent = "Order now";
 
 dish3.appendChild(dish3Heading);
