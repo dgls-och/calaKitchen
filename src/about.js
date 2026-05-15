394b54e41a5f9f8ccda0f46bddeec4e73ab9a566
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-15 18:18:24 +0100

    Add node elements to about.js

---
+++ src/about.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export const about = (function () {
+  const displayAbout = (display) => {
+    const heading = document.createElement("h2");
+    heading.textContent = "Let's Tell You the Story"
+    
+    const about = document.createElement("p");
+    about.textContent = "calaKitchen started with a simple idea: " 
+      + "food should feel like home, no matter where it's from. " 
+      + "By bringing together Nigerian classics, East african warmth, and Asian flair, " 
+      + "we've created a space where cultures meet on a plate. Our passion is "
+      + "serving meals that not only taste good but leave lasting memories.";
+
+    display.appendChild(heading);
+    display.appendChild(about);
+  };
+
+  return { displayAbout };
+})();
