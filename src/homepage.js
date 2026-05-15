390941196efdd897f39ac18da44e997174dc99d0
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-15 14:16:31 +0100

    Add home content menu style

--- src/homepage.js
+++ src/homepage.js
@@ -1,10 +1,30 @@
 const outputDiv = document.querySelector("#content");
 
-const heroHeading = document.createElement("h2"); 
-heroHeading.textContent = "Taste That Feels Like Home";
+const heroHeadline = document.createElement("h2");
+heroHeadline.textContent = "Taste That Feels Like Home";
 
-const heroDescription = document.createElement("p");
+const heroSubtext1 = document.createElement("p");
+heroSubtext1.textContent = "From rich local flavours to modern favourites, calaKitchen "
+  + "serves food that satisfies both heart and hunger.";
 
-const heroCTA = document.createElement("p");
+const heroSubtext2 = document.createElement("p");
+heroSubtext2.textContent = "Come hungry. Leave happy.";
 
-outputDiv.appendChild(heroHeading);
+const menuButton = document.createElement("button");
+menuButton.dataset.id = "menu";
+menuButton.textContent = "Check our menu";
+menuButton.classList.add("content-nav-button", "menu-button")
+
+const aboutButton = document.createElement("button");
+aboutButton.dataset.id = "about";
+aboutButton.textContent = "Learn more about us";
+aboutButton.classList.add("content-nav-button")
+
+const contentNav = document.createElement("nav");
+contentNav.appendChild(menuButton);
+contentNav.appendChild(aboutButton);
+
+outputDiv.appendChild(heroHeadline);
+outputDiv.appendChild(heroSubtext1);
+outputDiv.appendChild(heroSubtext2);
+outputDiv.appendChild(contentNav);
