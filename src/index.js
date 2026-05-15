cdf374fbe8daa3f73b878bcd679cc9430e3472b8
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-15 14:58:18 +0100

    Add logic to populate display at click of the home button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import { homepage } from "./homepage";
+
+const display = document.querySelector("#content");
+const navButtons = document.querySelectorAll("nav button");
+
+navButtons.forEach(button => {
+  button.addEventListener('click', e => {
+    e.preventDefault();
+    if (button.dataset.id == "home") {
+      homepage.displayContent(display);
+    }
+  });
+});
