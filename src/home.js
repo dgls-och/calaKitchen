ea3e9189f09732592dc358341470ee56252d1cf4
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-15 15:18:00 +0100

    Side import css so that webpack can handle styles

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 export const home = (function () {
   const displayContent = (outputDiv) => {
     const heroHeadline = document.createElement("h2");
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { home } from "./home";
+import "./styles.css";
+import { home } from "./home.js";
 
 const display = document.querySelector("#content");
 const navButtons = document.querySelectorAll("nav button");
@@ -7,6 +8,7 @@ navButtons.forEach(button => {
   button.addEventListener('click', e => {
     e.preventDefault();
     if (button.dataset.id == "home") {
+      display.textContent = "";
       home.displayContent(display);
     }
   });
